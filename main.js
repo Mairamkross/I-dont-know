0b19ccf499970fc3f1accadad2f6b8f03a1cc264
Mairamkross <dyikanbaevmairambek@gmail.com>
2023-01-13 12:52:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// let el = document.querySelector('#text1')
+// console.log(el);
+// let newEl = document.createElement('h1')
+// newEl.innerHTML = 'new element'
+// el.append(newEl)
+
+
+// function F1(){
+
+// }
+
+// let follow = document.querySelector('.styl1')
+// console.log(follow);
+
+// follow.onclick = ()=>{
+//     follow.classList.toggle('newClass')
+//     if (follow.innerHTML == 'потписаться'){
+//         follow.innerHTML = 'потписки'
+//     }else{
+//         follow.innerHTML = 'потписаться'
+//      }
+    
+// }
+
+
+// function folloF(){
+//    alert('!!!')
+// }
+
+// follow.onclick = folloF
+
+
+
+
+//input
+
+// let inp = document.querySelector('#inp1')
+// let btn = document.querySelector('#btn1')
+// let text = document.querySelector('#text')
+// btn.onclick = ()=>{
+//     console.log(inp.value);
+//     text.innerHTML = inp.value
+// }
+
+
+
+
+
+//задание
+//#1
+// let el = document.querySelector('#text1')
+// console.log(el);
+// let newEl = document.createElement("h1")
+//  newEl.innerHTML = 'erv'
+// el.before(newEl)
+
+// let newElem = document.createElement('h4')
+// newElem.innerHTML = 'ervs'
+// newEl.after(newElem)
+
+//#2
+// let follow = document.querySelector('#btn')
+// console.log(follow);
+// follow.onclick = ()=>{
+//     follow.classList.toggle('newClass')
+//     if(follow.innerHTML == 'light'){
+//         follow.innerHTML = 'dark'
+        
+//     }else{
+//         follow.innerHTML = 'light'
+//     }
+// }
+
+
+
+
+
+
+// let follow = document.querySelector('.styl1')
+// console.log(follow);
+
+// follow.onclick = ()=>{
+//     follow.classList.toggle('newClass')
+//     if (follow.innerHTML == 'потписаться'){
+//        follow.innerHTML = 'потписки'
+//     }else{
+//         follow.innerHTML = 'потписаться'
+//       }
+    
+// }
+
+
+
